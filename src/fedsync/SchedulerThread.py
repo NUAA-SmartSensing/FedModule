f5ba61f49833889e777fff80c320deb45f86013b
desperado <527707607@qq.com>
2022-06-28 16:48:05 +0800

    修复同步bug

--- src/fedsync/SchedulerThread.py
+++ src/fedsync/SchedulerThread.py
@@ -28,34 +28,39 @@ class SchedulerThread(threading.Thread):
         self.config = scheduler_config
 
     def run(self):
+        last_s_time = -1
         while self.current_t.get_time() < self.T:
-            # 每隔一段时间进行一次schedule
-            self.empty_sem.acquire()
-            self.mutex_sem.acquire()
             current_time = self.current_t.get_time()
-            print("| current_epoch |", current_time)
-            print("Begin client select")
-            selected_client_threads = self.client_select(self.config["params"])
-            print("\nSchedulerThread select(", len(selected_client_threads), "clients):")
-            self.server_thread_lock.acquire()
-            server_weights = copy.deepcopy(self.server_network.state_dict())
-            self.server_thread_lock.release()
-            for s_client_thread in selected_client_threads:
-                print(s_client_thread.get_client_id(), end=" | ")
-                # 将server的模型参数和时间戳发给client
-                s_client_thread.set_client_weight(server_weights)
-                s_client_thread.set_time_stamp(current_time)
+            if last_s_time != current_time:
+                # 每隔一段时间进行一次schedule
+                self.empty_sem.acquire()
+                self.mutex_sem.acquire()
+                if self.current_t.get_time() == self.T:
+                    break
+                print("| current_epoch |", current_time)
+                print("Begin client select")
+                last_s_time = current_time
+                selected_client_threads = self.client_select(self.config["params"])
+                print("\nSchedulerThread select(", len(selected_client_threads), "clients):")
+                self.server_thread_lock.acquire()
+                server_weights = copy.deepcopy(self.server_network.state_dict())
+                self.server_thread_lock.release()
+                for s_client_thread in selected_client_threads:
+                    print(s_client_thread.get_client_id(), end=" | ")
+                    # 将server的模型参数和时间戳发给client
+                    s_client_thread.set_client_weight(server_weights)
+                    s_client_thread.set_time_stamp(current_time)
 
-                # 启动一次client线程
-                s_client_thread.set_event()
-            del server_weights
-            print("\n-----------------------------------------------------------------Schedule complete")
-            # 等待所有客户端上传更新
-            self.receiver.start(len(selected_client_threads))
-            # 通知updater聚合权重
-            self.mutex_sem.release()
-            self.full_sem.release()
-            time.sleep(0.01)
+                    # 启动一次client线程
+                    s_client_thread.set_event()
+                del server_weights
+                print("\n-----------------------------------------------------------------Schedule complete")
+                # 等待所有客户端上传更新
+                self.receiver.start(len(selected_client_threads))
+                # 通知updater聚合权重
+                self.mutex_sem.release()
+                self.full_sem.release()
+                time.sleep(0.01)
 
     def client_select(self, params):
         current_checked_client_tl = self.sync_client_manager.get_checked_in_client_thread_list()
