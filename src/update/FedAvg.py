ae086c168eaa64dc8d78afda8224eac0762fea92
desperado <527707607@qq.com>
2022-06-30 15:33:43 +0800

    修复FedAvg Bug

--- src/update/FedAvg.py
+++ src/update/FedAvg.py
@@ -5,15 +5,15 @@ import torch
 
 class FedAvg:
     def update_server_weights(self, updater_thread: UpdaterThread, epoch, update_list, update_param):
-        client_nums = len(update_list)
+        total_nums = 0
+        for update_dict in update_list:
+            total_nums += update_dict["data_sum"]
         updated_parameters = {}
         for key, var in update_list[0]["weights"].items():
-            updated_parameters[key] = update_list[0]["weights"][key]
+            updated_parameters[key] = update_list[0]["weights"][key] * update_list[0]["data_sum"] / total_nums
         for i in range(len(update_list)-1):
             update_dict = update_list[i+1]
             client_weights = update_dict["weights"]
             for key, var in client_weights.items():
-                updated_parameters[key] += client_weights[key]
-        for key, var in updated_parameters.items():
-            updated_parameters[key] = updated_parameters[key] / client_nums
+                updated_parameters[key] += client_weights[key] * update_dict["data_sum"] / total_nums
         return updated_parameters
