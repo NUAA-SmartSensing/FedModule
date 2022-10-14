5defdc2be620e066bc10c404cf03e13d4848efcf
desperado <527707607@qq.com>
2022-10-14 15:56:30 +0800

    [main]将receiver和checker收纳进fedsync包里

--- src/fedsync/QueueManager.py
+++ src/fedsync/QueueManager.py
@@ -5,7 +5,7 @@ class QueueManager:
     def __init__(self, queue, current_time, config):
         self.queue = queue
         self.config = config
-        checker_class = ModuleFindTool.find_class_by_string("checker", config["checker_file"], config["checker_name"])
+        checker_class = ModuleFindTool.find_class_by_path(f'fedsync.checker.{config["checker_file"]}', config["checker_name"])
         self.checker = checker_class(current_time, config["params"])
 
     def put(self, update):
--- src/receiver/__init__.py
+++
@@ -1,1 +0,0 @@
-from receiver import AvgReceiver
