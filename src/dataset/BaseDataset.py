f273bb0db7eda05252ed12e2e6013c1bd35117d5
desperadoccy <527707607@qq.com>
2023-07-11 16:13:48 +0800

    [main]optimze BaseDataset's variables

--- src/dataset/BaseDataset.py
+++ src/dataset/BaseDataset.py
@@ -5,6 +5,8 @@ from utils.IID import generate_iid_data, generate_non_iid_data
 
 class BaseDataset:
     def __init__(self, iid_config):
+        self.label_min = None
+        self.label_max = None
         self.datasets = []
         self.iid_config = iid_config
         self.train_data_size = None
@@ -20,6 +22,8 @@ class BaseDataset:
         self.test_datasets = test_datasets
         self.train_data = train_datasets.data
         self.test_data = test_datasets.data
+        self.label_max = self.train_labels.max()
+        self.label_min = self.train_labels.min()
 
         self.train_data_size = self.train_data.shape[0]
 
@@ -28,7 +32,7 @@ class BaseDataset:
             generate_iid_data(self, clients, train_datasets)
         else:
             print("generating non_iid data...")
-            generate_non_iid_data(self.iid_config, self, clients, self.train_labels.min(), self.train_labels.max() + 1,
+            generate_non_iid_data(self.iid_config, self, clients, self.label_min, self.label_max + 1,
                                   train_datasets)
         print("data generation process completed")
 
