04181da9fef4f2dea856896cc6b6d8dbf2d39106
desperadoccy <527707607@qq.com>
2023-07-30 14:30:53 +0800

    [main]add a data-structure

---
+++ src/utils/Structures.py
@@ -0,0 +1,17 @@
+class BidirectionalMappedDict:
+    def __init__(self):
+        self.group_to_data = {}
+        self.id_to_group = {}
+
+    def add_data(self, data_id, group_num, data):
+        if group_num not in self.group_to_data:
+            self.group_to_data[group_num] = []
+
+        self.group_to_data[group_num].append((data_id, data))
+        self.id_to_group[data_id] = group_num
+
+    def get_group(self, group_num):
+        return self.group_to_data[group_num]
+
+    def get_group_for_id(self, data_id):
+        return self.id_to_group[data_id]
