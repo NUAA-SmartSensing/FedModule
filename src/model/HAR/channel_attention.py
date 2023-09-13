b594def5978642e0a61573f5c7c66419c7ac9cc8
desperadoccy <527707607@qq.com>
2023-09-13 20:49:28 +0800

    [main]add some models about HAR

--- src/model/HAR/channel_attention.py
+++ src/model/HAR/channel_attention.py
@@ -1,27 +1,30 @@
 import torch.nn as nn
 
 '''Channel Attention Neural Network: 通道注意力'''
+
+
 class ChannelAttentionModule(nn.Module):
     def __init__(self, inchannel):
         super().__init__()
         self.att_fc = nn.Sequential(
-            nn.Linear(inchannel, inchannel//4),
+            nn.Linear(inchannel, inchannel // 4),
             nn.ReLU(),
-            nn.Linear(inchannel//4, inchannel),
+            nn.Linear(inchannel // 4, inchannel),
             nn.Sigmoid()
         )
+
     def forward(self, x):
         '''
             x.shape: [b, c, series, modal]
         '''
         # 传感数据特殊性，固定模态轴，只在时序轴上做GAP或者GMP
-        att = nn.AdaptiveAvgPool2d((1, x.size(-1)))(x) # [b, c, series, modal] -> [b, c, 1, modal]
-        att = att.permute(0, 3, 1, 2).squeeze(-1) # [b, c, 1, modal] -> [b, modal, c]
+        att = nn.AdaptiveAvgPool2d((1, x.size(-1)))(x)  # [b, c, series, modal] -> [b, c, 1, modal]
+        att = att.permute(0, 3, 1, 2).squeeze(-1)  # [b, c, 1, modal] -> [b, modal, c]
         att = self.att_fc(att)  # [b, modal, c]
-        att = att.permute(0, 2, 1).unsqueeze(-2) # [b, modal, c] -> [b, c, modal] -> [b, c, 1, modal]
+        att = att.permute(0, 2, 1).unsqueeze(-2)  # [b, modal, c] -> [b, c, modal] -> [b, c, 1, modal]
         out = x * att
         return out
-    
+
 
 class ChannelAttentionNeuralNetwork(nn.Module):
     def __init__(self, train_shape, category):
@@ -52,7 +55,7 @@ class ChannelAttentionNeuralNetwork(nn.Module):
             nn.ReLU()
         )
         self.ada_pool = nn.AdaptiveAvgPool2d((1, train_shape[-1]))
-        self.fc = nn.Linear(512*train_shape[-1], category)
+        self.fc = nn.Linear(512 * train_shape[-1], category)
 
     def forward(self, x):
         '''
--- src/model/__init__.py
+++ src/model/__init__.py
@@ -1,2 +1,3 @@
 from model import CNN
 from model import LeNet5
+from model import HAR
