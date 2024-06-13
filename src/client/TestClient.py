6192f1505032036fd11b55e14b5128a1d4affd48
desperadoccy <527707607@qq.com>
2024-06-13 17:39:50 +0800

    fix the bug about config not found

--- src/client/TestClient.py
+++ src/client/TestClient.py
@@ -6,10 +6,11 @@ class TestClient(NormalClient, TestableMixin):
     def __init__(self, c_id, stop_event, selected_event, delay, index_list, config, dev):
         NormalClient.__init__(self, c_id, stop_event, selected_event, delay, index_list, config, dev)
         TestableMixin.__init__(self, config)
+        self.global_config = None
 
     def run(self):
         super().run()
-        experiment = self.global_var["global_config"]['experiment']
+        experiment = self.global_config['experiment']
         self.save_test_record(self.client_id, experiment)
 
     def train(self):
@@ -23,3 +24,7 @@ class TestClient(NormalClient, TestableMixin):
                        "accuracy": self.accuracy_list[len(self.accuracy_list) - 1],
                        "loss": self.loss_list[len(self.loss_list) - 1]}
         self.message_queue.put_into_uplink(update_dict)
+
+    def init_client(self):
+        super().init_client()
+        self.global_config = self.message_queue.get_config_by_key('global_config')
