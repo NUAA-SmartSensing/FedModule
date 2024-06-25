55c1eaaa04f667e0fb246658e1e740acb1a18cc4
desperadoccy <527707607@qq.com>
2024-06-25 22:01:52 +0800

    fix out of range

--- src/groupmanager/NormalGroupManager.py
+++ src/groupmanager/NormalGroupManager.py
@@ -8,16 +8,17 @@ class NormalGroupManager(BaseGroupManager):
         super().__init__(config)
         self.client_list = self.global_var['client_id_list']
         self.latency_list = self.global_var['client_staleness_list']
-        self.network_list = []
-        self.group_client_num_list = []
+        self.network_list = [0] * len(self.client_list)
+        self.group_client_num_list = [0] * len(self.client_list)
         self.group_method = ModuleFindTool.find_class_by_path(self.config["group_method"]["path"])(self, self.config[
             "group_method"]["params"])
         self.group_caller = GroupCaller(self)
         self.group_list, self.group_num = self.group_caller.group(self.client_list, self.latency_list)
-        self.epoch_list = [0 for _ in range(self.global_var['global_config']['client_num'])]
+        self.epoch_list = [0] * len(self.client_list)
 
-    def __group(self, client_list, latency_list):
-        return self.group_caller.group(client_list, latency_list)
+    def __group(self, client_list, latency_list, *args, **kwargs):
+        self.group_list, self.group_num = self.group_caller.group(client_list, latency_list, *args, **kwargs)
+        return self.group_list, self.group_num
 
     def get_group_num(self):
         return self.group_num
@@ -25,6 +26,5 @@ class NormalGroupManager(BaseGroupManager):
     def get_group_list(self):
         return self.group_list
 
-    def update(self):
-        self.__group(self.client_list, self.latency_list)
-        return self.group_num
+    def update(self, *args, **kwargs):
+        return self.__group(self.client_list, self.latency_list, *args, **kwargs)
