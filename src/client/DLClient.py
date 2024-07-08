86e2adc09ac37f24c16568f7652b9a7a0ff73478
desperadoccy <527707607@qq.com>
2024-07-08 16:42:06 +0800

    fix DL loading bug

--- src/client/DLClient.py
+++ src/client/DLClient.py
@@ -10,23 +10,24 @@ class DLClient(TestClient):
 
     def receive_notify(self):
         if self.message_queue.get_from_downlink(self.client_id, 'received_weights'):
+            if self.training_params is None:
+                self.training_params = self.message_queue.get_training_params()
             self.message_queue.put_into_downlink(self.client_id, 'received_weights', False)
-            self.weights_buffer = self.message_queue.get_from_downlink(self.client_id, 'weights_buffer')
+            weights_buffer = self.message_queue.get_from_downlink(self.client_id, 'weights_buffer')
             # 更新模型参数
             if self.init:
-                if self.client_id in self.weights_buffer.keys():
+                if self.client_id in weights_buffer.keys():
                     for key, var in self.model.state_dict().items():
                         if self.training_params[key]:
                             if torch.cuda.is_available():
-                                self.weights_buffer[self.client_id][key] = self.weights_buffer[self.client_id][key].to(
+                                weights_buffer[self.client_id][key] = weights_buffer[self.client_id][key].to(
                                     self.dev)
-                            self.weights_buffer[self.client_id][key] = self.config['alpha'] * var + (
-                                    1 - self.config['alpha']) * self.weights_buffer[self.client_id][key]
-                    self.model.load_state_dict(self.weights_buffer[self.client_id], strict=True)
+                            weights_buffer[self.client_id][key] = self.config['alpha'] * var + (
+                                    1 - self.config['alpha']) * weights_buffer[self.client_id][key]
+                    self.model.load_state_dict(weights_buffer[self.client_id], strict=True)
             else:
-                self.model.load_state_dict(self.weights_buffer['global'], strict=True)
+                self.model.load_state_dict(weights_buffer['global'], strict=True)
                 self.init = True
-            self.received_weights = False
         if self.message_queue.get_from_downlink(self.client_id, 'received_time_stamp'):
             self.message_queue.put_into_downlink(self.client_id, 'received_time_stamp', False)
             self.time_stamp = self.message_queue.get_from_downlink(self.client_id, 'time_stamp_buffer')
