01e259920cfd16e4488eccaff8e758a06b7bfe5c
desperadoccy <527707607@qq.com>
2024-07-08 17:01:24 +0800

    add some clients about conutinue and online learnig

--- src/client/StreamClient.py
+++ src/client/StreamClient.py
@@ -1,7 +1,16 @@
+import copy
+from copy import deepcopy
+
+import torch
+from torch import nn
 from torch.utils.data import DataLoader
 
 from client.NormalClient import NormalClient
+from utils import ModuleFindTool
 from utils.DatasetUtils import FLDataset
+from utils.IID import generate_iid_data, generate_non_iid_data
+from utils.Tools import to_dev, to_cpu
+from torch.nn import functional as F
 
 
 class StreamClient(NormalClient):
@@ -13,7 +22,8 @@ class StreamClient(NormalClient):
         self.total_epoch = 0
 
     def change_task(self):
-        self.fl_train_ds = FLDataset(self.train_ds, list(self.index_list)[self.task_id::self.task_num], self.transform, self.target_transform)
+        self.fl_train_ds = FLDataset(self.train_ds, list(self.index_list)[self.task_id::self.task_num], self.transform,
+                                     self.target_transform)
         self.train_dl = DataLoader(self.fl_train_ds, batch_size=self.batch_size, shuffle=True, drop_last=True)
         self.task_id = (self.task_id + 1) % self.task_num
 
@@ -23,3 +33,169 @@ class StreamClient(NormalClient):
             self.change_task()
         super().local_task()
         self.total_epoch += 1
+
+
+class StreamClientWithGlobal(StreamClient):
+    def change_task(self):
+        self.fl_train_ds = FLDataset(self.train_ds, list(self.index_list[self.task_id]), self.transform,
+                                     self.target_transform)
+        self.train_dl = DataLoader(self.fl_train_ds, batch_size=self.batch_size, shuffle=True, drop_last=True)
+        self.task_id = (self.task_id + 1) % self.task_num
+
+
+class StreamClientWithDir(StreamClient):
+    def __init__(self, c_id, stop_event, selected_event, delay, index_list, config, dev):
+        super().__init__(c_id, stop_event, selected_event, delay, index_list, config, dev)
+        self.task_index_list = [[] for _ in range(self.task_num)]
+        self.previous_model = {}
+
+    def init_client(self):
+        super().init_client()
+        iid_config = self.config["iid"]
+        train_labels = self.train_ds.targets[self.index_list]
+        if isinstance(iid_config, bool):
+            index_list = generate_iid_data(train_labels, self.task_num)
+        elif isinstance(iid_config, dict) and "path" in iid_config:
+            data_distribution_generator = ModuleFindTool.find_class_by_path(iid_config["path"])(iid_config["params"])
+            index_list = data_distribution_generator.generate_data(iid_config, train_labels, self.task_num,
+                                                                   self.fl_train_ds)
+        else:
+            index_list = generate_non_iid_data(iid_config, train_labels, self.task_num)
+        self.task_index_list = index_list
+
+
+class ContinualClient(StreamClientWithGlobal):
+    def __init__(self, c_id, stop_event, selected_event, delay, index_list, config, dev):
+        super().__init__(c_id, stop_event, selected_event, delay, index_list, config, dev)
+        self.label_mapping = None
+
+    def init_client(self):
+        super().init_client()
+        self.label_mapping = self.message_queue.get_from_downlink(self.client_id, "label_mapping")
+
+    def train_one_epoch(self):
+        if self.mu != 0:
+            global_model = deepcopy(self.model)
+        data_sum = 0
+        for epoch in range(self.epoch):
+            for data, label in self.train_dl:
+                label = torch.LongTensor([self.label_mapping[i.item()] for i in label])
+                data, label = data.to(self.dev), label.to(self.dev)
+                preds = self.model(data)
+                # Calculate the loss function
+                loss = self.loss_func(preds, label)
+                data_sum += label.size(0)
+                # proximal term
+                if self.mu != 0:
+                    proximal_term = 0.0
+                    for w, w_t in zip(self.model.parameters(), global_model.parameters()):
+                        proximal_term += (w - w_t).norm(2)
+                    loss = loss + (self.mu / 2) * proximal_term
+                # backpropagate
+                loss.backward()
+                # Update the gradient
+                self.opti.step()
+                if self.lr_scheduler:
+                    self.lr_scheduler.step()
+                # Zero out the gradient and initialize the gradient.
+                self.opti.zero_grad()
+        # Return the updated model parameters obtained by training on the client's own data.
+        weights = self.model.state_dict()
+        torch.cuda.empty_cache()
+        return data_sum, weights
+
+
+class ContinualClientWithEWC(ContinualClient):
+    def __init__(self, c_id, stop_event, selected_event, delay, index_list, config, dev):
+        super().__init__(c_id, stop_event, selected_event, delay, index_list, config, dev)
+        self.previous_model = None
+        self.fisher = None
+        self.ewc_lambda = 1000
+
+    def receive_notify(self):
+        if self.total_epoch % self.task_interval == 0:
+            if self.fisher is None and self.task_id == 0:
+                pass
+            else:
+                del self.fisher
+                del self.previous_model
+                self.previous_model = deepcopy({n: p for n, p in self.model.named_parameters() if p.requires_grad})
+                self.compute_fisher(self.train_dl)
+                print(f"client {self.client_id} saved task model.")
+        super().receive_notify()
+
+    def train_one_epoch(self):
+        if self.mu != 0:
+            global_model = deepcopy(self.model)
+        data_sum = 0
+        self.model.train()
+        for epoch in range(self.epoch):
+            for data, label in self.train_dl:
+                label = torch.LongTensor([self.label_mapping[i.item()] for i in label])
+                data, label = data.to(self.dev), label.to(self.dev)
+                preds = self.model(data)
+                # Calculate the loss function
+                loss = self.loss_func(preds, label)
+                data_sum += label.size(0)
+                # proximal term
+                if self.mu != 0:
+                    proximal_term = 0.0
+                    for w, w_t in zip(self.model.parameters(), global_model.parameters()):
+                        proximal_term += (w - w_t).norm(2)
+                    loss = loss + (self.mu / 2) * proximal_term
+                if self.fisher is not None:
+                    l = self.ewc_loss()
+                    print(l)
+                    loss += l
+                # backpropagate
+                loss.backward()
+                # Update the gradient
+                self.opti.step()
+                if self.lr_scheduler:
+                    self.lr_scheduler.step()
+                # Zero out the gradient and initialize the gradient.
+                self.opti.zero_grad()
+        # Return the updated model parameters obtained by training on the client's own data.
+        weights = self.model.state_dict()
+        torch.cuda.empty_cache()
+        return data_sum, weights
+
+    def compute_fisher(self, data_loader, num_samples=100):
+        """
+        Compute the Fisher information matrix for the current model
+
+        Args:
+            data_loader: PyTorch DataLoader for the training data
+            num_samples: number of samples to use for computing the Fisher matrix
+        """
+        fisher = {}
+        params = {n: p for n, p in self.model.named_parameters() if p.requires_grad}
+        for n, p in params.items():
+            fisher[n] = torch.zeros_like(p.data, requires_grad=False)
+        self.model.eval()
+        # 计算fisher矩阵
+        for i, (inputs, targets) in enumerate(data_loader):
+            if i >= num_samples:
+                break
+            inputs, targets = inputs.to(self.dev), targets.to(self.dev)
+            self.model.zero_grad()
+            outputs = self.model(inputs)
+            loss = F.cross_entropy(outputs, targets)
+            loss.backward()
+            for name, param in self.model.named_parameters():
+                fisher[name] += param.grad.data.pow(2) / num_samples
+        # 使用fisher矩阵计算ewc loss
+        self.fisher = {n: p for n, p in fisher.items()}
+
+    def ewc_loss(self):
+        """
+        Calculate the EWC loss based on the Fisher information matrix
+
+        Returns:
+            EWC loss: a scalar value representing the EWC loss
+        """
+        loss = 0
+        for name, param in self.model.named_parameters():
+            loss += torch.sum(self.fisher[name] * (param.data - self.previous_model[name]).pow(2))
+        loss = (self.ewc_lambda / 2) * loss
+        return loss
