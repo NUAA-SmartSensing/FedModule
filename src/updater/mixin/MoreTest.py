52403dec5260d4222b501aadc1acb31eaf2d921b
desperadoccy <527707607@qq.com>
2024-07-31 21:08:55 +0800

    add function of saving results

--- src/updater/mixin/MoreTest.py
+++ src/updater/mixin/MoreTest.py
@@ -1,3 +1,5 @@
+import os
+
 import numpy as np
 import torch
 import wandb
@@ -5,9 +7,13 @@ from torch.utils.data import DataLoader
 
 from utils.DatasetUtils import FLDataset
 from utils.GlobalVarGetter import GlobalVarGetter
+from utils.Tools import saveAns
 
 
 class TestEachClass:
+    def __init__(self):
+        self.results = []
+
     def run_server_test(self, epoch):
         dl = DataLoader(self.test_data, batch_size=100, shuffle=True, drop_last=True)
         test_correct = 0
@@ -33,12 +39,24 @@ class TestEachClass:
             loss = test_loss / len(dl)
             self.loss_list.append(loss)
             self.accuracy_list.append(accuracy)
+            epoch_result = {}
             print('Epoch(t):', epoch, 'accuracy:', accuracy, 'loss', loss)
             for i in range(num_classes):
-                print(f"acc on class {i}: {class_accuracies[i]/class_total[i]:.4f}")
+                acc = class_accuracies[i] / class_total[i]
+                epoch_result[i] = acc
+                print(f"acc on class {i}: {acc:.4f}")
+            self.results.append(epoch_result)
             if self.config['enabled']:
-                wandb.log({'accuracy': accuracy, 'loss': loss})
+                wandb.log({'accuracy': accuracy, 'loss': loss, 'class_acc': epoch_result})
         return accuracy, loss
+
+    def __del__(self):
+        global_var = GlobalVarGetter.get()
+        saveAns(os.path.join(os.path.dirname(os.path.abspath(__file__)), "../results/",
+                             global_var['global_config']["experiment"],
+                             "class_acc.txt"), self.results)
+        if self.config['enabled']:
+            wandb.save(os.path.join(wandb.run.dir, "class_acc.txt"), self.results)
 
 
 class TestMultiTask:
@@ -83,8 +101,7 @@ class TestMultiTask:
             self.accuracy_list.append(accuracy)
             print(f'Epoch(t): {epoch}-{task} accuracy: {accuracy} {loss}')
             for i in range(num_classes):
-                print(f"acc on class {i}: {class_accuracies[i]/class_total[i]:.4f}")
+                print(f"acc on class {i}: {class_accuracies[i] / class_total[i]:.4f}")
             if self.config['enabled']:
-                wandb.log({'task': task,'accuracy': accuracy, 'loss': loss})
+                wandb.log({'task': task, 'accuracy': accuracy, 'loss': loss})
         return accuracy, loss
-
