49de17bc92478c007a2acd4c5d5954a52094ba5b
desperadoccy <527707607@qq.com>
2024-08-02 11:07:11 +0800

    add a new modified resnet for MNIST

--- src/model/torch.py
+++ src/model/torch.py
@@ -10,3 +10,14 @@ class ResNet18Pre(nn.Module):
 
     def forward(self, x):
         return self.model(x)
+
+
+class ResNet18ForOneTunnel(nn.Module):
+    def __init__(self, num_classes=10, pretrained=False):
+        super(ResNet18ForOneTunnel, self).__init__()
+        self.model = models.resnet18(pretrained=pretrained)
+        self.model.conv1 = nn.Conv2d(1, 64, kernel_size=7, stride=2, padding=3, bias=False)
+        self.model.fc = nn.Linear(self.model.fc.in_features, num_classes)
+
+    def forward(self, x):
+        return self.model(x)
