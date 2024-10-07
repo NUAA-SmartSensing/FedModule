46ce9bbec2f3912d02ccdd31e1328ddf41d587ef
desperadoccy <527707607@qq.com>
2024-10-07 20:38:03 +0800

    imporve module loader

--- src/utils/ModuleFindTool.py
+++ src/utils/ModuleFindTool.py
@@ -1,10 +1,16 @@
 def find_class_by_path(path: str):
     path_list = path.split(".")
-    module = __import__(path_list[0])
-    path_list = path_list[1:]
-    for package in path_list:
-        module = getattr(module, package)
-    return module
+    for i in range(len(path_list)):
+        entry = '.'.join(path_list[:i+1])
+        module = __import__(entry)
+        attr_list = path_list[i:] if i > 0 else path_list[i+1:]
+        try:
+            for package in attr_list:
+                module = getattr(module, package)
+            return module
+        except:
+            pass
+    raise Exception(f"Module {path} not found.")
 
 
 def generate_object_by_path(path: str, params: dict, else_params=None):
