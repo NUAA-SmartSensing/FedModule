41736ae60c3130bc5adfe2555f14ced6484dc07d
desperadoccy <527707607@qq.com>
2024-10-22 17:13:38 +0800

    remove useless function

--- src/client/ActiveClient.py
+++ src/client/ActiveClient.py
@@ -9,19 +9,6 @@ class ActiveClient(NormalClient):
         self.group_id = None
         self.acquire_model_delay = config['acquire_model_delay']
 
-    def run(self):
-        """
-        When it is selected by the server, the client starts local training and never stops until stop event is set.
-        """
-        self.init_client()
-        while not self.stop_event.is_set():
-            # The client is selected and starts local training.
-            if self.event.is_set():
-                self.local_run()
-            # The client waits to be selected.
-            else:
-                self.event.wait()
-
     def local_run(self):
         """
         The run function of Client runs the main body, suitable for use as a target parameter of process.
--- src/client/TestClient.py
+++ src/client/TestClient.py
@@ -8,12 +8,6 @@ class TestClient(NormalClient, TestableMixin):
         TestableMixin.__init__(self, config)
         self.global_config = None
 
-    def run(self):
-        super().run()
-        if self.global_config['save']:
-            experiment = self.global_config['experiment']
-            self.save_test_record(self.client_id, experiment)
-
     def train(self):
         data_sum, weights = super().train()
         self.run_test(self.client_id, self.time_stamp, self.model, self.loss_func, self.dev)
@@ -29,3 +23,8 @@ class TestClient(NormalClient, TestableMixin):
     def init_client(self):
         super().init_client()
         self.global_config = self.message_queue.get_config_by_key('global_config')
+
+    def finish_client(self):
+        if self.global_config['save']:
+            experiment = self.global_config['experiment']
+            self.save_test_record(self.client_id, experiment)
