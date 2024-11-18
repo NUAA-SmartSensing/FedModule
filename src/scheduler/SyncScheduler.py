d0d717f4a215281b342d9f40f4a56e678d04407e
desperadoccy <527707607@qq.com>
2024-11-18 21:31:19 +0800

    add a new scheduler using chain of responsibilty

--- src/scheduler/SyncScheduler.py
+++ src/scheduler/SyncScheduler.py
@@ -1,5 +1,7 @@
 import time
 
+from core.handlers.Handler import HandlerChain
+from core.handlers.ServerHandler import ClientSelector, ContentDispatcher, UpdateWaiter
 from scheduler.BaseScheduler import BaseScheduler
 
 
@@ -45,3 +47,65 @@ class SyncScheduler(BaseScheduler):
             self.selected_event_list[client_id].set()
         print("\n-----------------------------------------------------------------Schedule complete")
         self.schedule_t.time_add()
+
+
+class HandlerChainScheduler(BaseScheduler):
+    def __init__(self, server_thread_lock, config, mutex_sem, empty_sem, full_sem):
+        BaseScheduler.__init__(self, server_thread_lock, config)
+        self.handler_chain = None
+        self.mutex_sem = mutex_sem
+        self.empty_sem = empty_sem
+        self.full_sem = full_sem
+        self.model = self.server_network
+        self.finals = []
+        self.download_dict = {}
+
+    def run(self):
+        self.handler_chain = self.create_handler_chain()
+        while self.current_t.get_time() <= self.T:
+            # Scheduling is performed periodically.
+            self.empty_sem.acquire()
+            self.mutex_sem.acquire()
+            self.execute_chain()
+            self.schedule_t.time_add()
+            # Notifying the updater to aggregate weights.
+            self.mutex_sem.release()
+            self.full_sem.release()
+            time.sleep(0.01)
+
+    def create_handler_chain(self):
+        chain = HandlerChain()
+        (chain.set_chain(ClientSelector())
+         .set_next(ContentDispatcher())
+         .set_next(UpdateWaiter()))
+        return chain
+
+    def execute_chain(self):
+        epoch = self.current_t.get_time()
+        request = {"epoch": epoch, "updater": self, "global_var": self.global_var,
+                   'scheduler': self.global_var['scheduler']}
+        self.handler_chain.handle(request)
+
+    def _final_output(self):
+        for params, func in self.finals:
+            func(params)
+
+    def add_final_output(self, params, func):
+        immutable_types = (int, float, str, tuple, bool, type(None))
+        if isinstance(params, immutable_types):
+            raise ValueError("params should be a mutable type")
+        self.finals.append((params, func))
+
+    def customize_download(self):
+        pass
+
+    def download_item(self, target, k, v):
+        if target not in self.download_dict:
+            self.download_dict[target] = {}
+        self.download_dict[target][k] = v
+
+    def notify_client(self):
+        self.customize_download()
+        for target, download_dict in self.download_dict.items():
+            for k, v in download_dict.items():
+                self.message_queue.put_into_downlink(target, k, v)
